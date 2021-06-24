8f05f180a6cd8dd4e0c74c818e491e4fc91d08e8
rhuangabrielsantos <rhuan.santos@db1.com.br>
2021-06-24 14:42:25 -0300

    :sparkles: Created Google Authentication with Firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from "react-router-dom";
 
+import { useAuth } from "../hooks/useAuth";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
@@ -10,8 +12,13 @@ import { Button } from "../components/Button";
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function handleCreateRoom() {
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
     history.push("/rooms/new");
   }
 
